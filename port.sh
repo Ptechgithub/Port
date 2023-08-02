f976b525e13aad4ee6f35e12932be73d61957e9e
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-02 13:36:47 +0330

    update port.sh

--- port.sh
+++ port.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Read the new SSH port number entered by the user
-read -p "Please enter a new SSH port number: " new_port
-
 # Get the current SSH port from the SSH configuration file
 current_port=$(grep -Eo '^[^#]*Port[[:space:]]+[0-9]+' /etc/ssh/sshd_config | awk '{print $2}')
+
+echo "The current SSH port is: $current_port"
+
+# Read the new SSH port number entered by the user
+read -p "Please enter a new SSH port number: " new_port
 
 # Backup the original SSH configuration file
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
@@ -15,4 +17,4 @@ sed -i "s/Port $current_port/Port $new_port/g" /etc/ssh/sshd_config
 # Restart the SSH service
 systemctl restart sshd.service
 
-echo "The original port $current_port is invalid. The SSH port has been changed to $new_port. Please log in with the new port. If you cannot log in, please check whether the firewall allows $new_port."
+echo "The SSH port has been changed from $current_port to $new_port. Please log in with the new port. If you cannot log in, please check whether the firewall allows $new_port."
