ff3ee8723f097044e4c6ebde6ce8dda24be2dcd8
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-02 14:48:56 +0330

    update port.sh

--- port.sh
+++ port.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Get the current SSH port from the SSH configuration file
-current_port=$(grep -Eo '^[^#]*Port[[:space:]]+[0-9]+' /etc/ssh/sshd_config | awk '{print $2}')
+# Get the current SSH ports from the SSH configuration file
+current_ports=($(grep -Eo '^[^#]*Port[[:space:]]+[0-9]+' /etc/ssh/sshd_config | awk '{print $2}'))
 
-echo "Your SSH port is: $current_port"
+# Display the current SSH ports
+if [ "${#current_ports[@]}" -eq 1 ]; then
+    echo "Your SSH port is: ${current_ports[0]}"
+else
+    echo "Your SSH ports are: ${current_ports[@]}"
+fi
 
 # Read the new SSH port number entered by the user
-read -p "Please enter a new SSH port : " new_port
+read -p "Please enter a new SSH port: " new_port
 
-# Check if the new port is different from the current port
-if [ "$new_port" -eq "$current_port" ]; then
+# Check if the new port is different from the current ports
+while [[ " ${current_ports[*]} " == *" $new_port "* ]]; do
     echo "The new port cannot be the same as the current port. Please enter a different SSH port."
-    read -p "Please enter a new SSH port : " new_port
-fi
+    read -p "Please enter a new SSH port: " new_port
+done
 
 # Open the new SSH port in the firewall
 sudo ufw allow $new_port
@@ -22,9 +27,11 @@ echo "Port $new_port is now allowed in the firewall."
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
 # Modify the port number in the SSH configuration file
-sed -i "s/Port $current_port/Port $new_port/g" /etc/ssh/sshd_config
+for port in "${current_ports[@]}"; do
+    sed -i "s/Port $port/Port $new_port/g" /etc/ssh/sshd_config
+done
 
 # Restart the SSH service
 sudo systemctl restart sshd.service
 
-echo "The SSH port has been changed from $current_port to $new_port. Please log in with the new port. If you cannot log in, please check whether the firewall allows $new_port."
+echo "The SSH ports have been changed to $new_port. Please log in with the new port. If you cannot log in, please check whether the firewall allows $new_port."
