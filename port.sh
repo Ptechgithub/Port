4b60026620de6d04c9603fbae401bae2d98202e0
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-02 17:48:15 +0330

    update port.sh

--- port.sh
+++ port.sh
@@ -12,6 +12,8 @@ read -p "Please enter a new SSH port (1_65535): " new_port
 # Backup the original SSH configuration file
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
+sudo ufw allow $new_port
+
 # Remove the # sign from the beginning of the Port line (if present)
 sed -i "s/^#Port/Port/" /etc/ssh/sshd_config
 
