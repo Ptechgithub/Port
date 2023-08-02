6f8d04649de27f97a9b9c3122e6793ad1d1d7544
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-02 14:40:19 +0330

    update port.sh

--- port.sh
+++ port.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# Get the current SSH port from the SSH configuration file
+current_port=$(grep -Eo '^[^#]*Port[[:space:]]+[0-9]+' /etc/ssh/sshd_config | awk '{print $2}')
+
+echo "Your SSH port is: $current_port"
+
 # Read the new SSH port number entered by the user
 read -p "Please enter a new SSH port number: " new_port
 
 # Open the new SSH port in the firewall
 sudo ufw allow $new_port
 echo "Port $new_port is now allowed in the firewall."
-
-# Get the current SSH port from the SSH configuration file
-current_port=$(grep -Eo '^[^#]*Port[[:space:]]+[0-9]+' /etc/ssh/sshd_config | awk '{print $2}')
-
-echo "Your SSH port is: $current_port"
 
 # Backup the original SSH configuration file
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
