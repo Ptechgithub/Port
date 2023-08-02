c8c66f8fd02d369768c197cbebb82dbc02f77c21
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-02 17:25:58 +0330

    update port.sh

--- port.sh
+++ port.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 
-# Get the current SSH ports from the SSH configuration file (including lines starting with #Port)
-current_ports_with_comments=($(grep -Eo '^[[:space:]]*#Port[[:space:]]+[0-9]+' /etc/ssh/sshd_config | awk '{print $2}'))
+# Get the current SSH port number
+current_port=$(grep -oP "(?<=Port ).*" /etc/ssh/sshd_config)
 
-# Remove # from the beginning of the ports
-current_ports=()
-for port in "${current_ports_with_comments[@]}"; do
-    current_ports+=("${port/#/}")
-done
-
-# Display the current SSH ports
-if [ "${#current_ports[@]}" -eq 1 ]; then
-    echo "Your SSH port is: ${current_ports[0]}"
-elif [ "${#current_ports[@]}" -gt 1 ]; then
-    echo "Your SSH ports are: ${current_ports[@]}"
-else
-    echo "No valid SSH port found in the configuration file."
-fi
+# Display the current SSH port
+echo "Current SSH port: $current_port"
 
 # Read the new SSH port number entered by the user
-read -p "Please enter a new SSH port: " new_port
-
-# Check if the new port is different from the current ports
-while [[ " ${current_ports[*]} " == *" $new_port "* ]]; do
-    echo "The new port cannot be the same as the current port. Please enter a different SSH port."
-    read -p "Please enter a new SSH port: " new_port
-done
-
-# Open the new SSH port in the firewall
-sudo ufw allow $new_port
-echo "Port $new_port is now allowed in the firewall."
+read -p "Please enter a new SSH port number: " new_port
 
 # Backup the original SSH configuration file
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
 # Modify the port number in the SSH configuration file
-for port in "${current_ports[@]}"; do
-    sed -i "s/Port $port/Port $new_port/g" /etc/ssh/sshd_config
-done
+sed -i "s/Port $current_port/Port $new_port/g" /etc/ssh/sshd_config
 
 # Restart the SSH service
-sudo systemctl restart sshd.service
+systemctl restart sshd.service
 
-echo "The SSH ports have been changed to $new_port. Please log in with the new port. If you cannot log in, please check whether the firewall allows $new_port."
+echo "The SSH port has been changed from $current_port to $new_port. Please log in with the new port. If you cannot log in, please check whether the firewall allows $new_port"
